28921b115ba31f6d02a04111e63e5b0ca17bde1c
AnMiKa <alphasierra@gmx.de>
2017-04-27 11:40:39 +0200

    Kapitel schreiben Änderung Q

--- Algorithmen/Noll Variable Metric/BFGS_update.m
+++ Algorithmen/Noll Variable Metric/BFGS_update.m
@@ -1,7 +1,25 @@
 % Noll BFGS
 function [ Q ] = BFGS_update(d,gm,gp,Q)
 
+% bound for Q
+bound = 1e5;
+
 y = gp-gm;
-Q = Q+(y*y')/(y'*d)-((Q*d)*(Q*d)')/(d'*Q*d);
+% corrections to keep Q bounded
+if norm((y*y')/(y'*d),'fro') > 1/3*bound
+    fro = norm(y*y','fro');
+    Q1 = (y*y')/(fro/(1/3*bound));
+else
+    Q1 = (y*y')/(y'*d);
+end
+
+if norm(((Q*d)*(Q*d)')/(d'*Q*d),'fro') > 1/3*bound
+    fro = norm(((Q*d)*(Q*d)'),'fro');
+    Q2 = ((Q*d)*(Q*d)')/(fro/(1/3*bound));
+else
+    Q2 = ((Q*d)*(Q*d)')/(d'*Q*d);
+end
+    
+Q = Q + Q1 - Q2;
 
 end
